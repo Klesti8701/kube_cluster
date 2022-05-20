109f13a1ff51e6630228656b4766f4fa1c9bf19c
X-Dean <deanxaka@gmail.com>
2022-05-20 13:12:32 +0200

    adding scripts for master and worker node

---
+++ k8s_worker.sh
@@ -0,0 +1,47 @@
+#/bin/bash
+
+swapoff -a
+sed -i 's/^/dev/mapper/almalinux-swap none                    swap    defaults        0 0_#/dev/mapper/almalinux-swap none                    swap    defaults        0 0' /etc/fstab
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+dnf install -y iproute-tc
+
+
+
+	firewall-cmd --permanent --add-port=10250/tcp
+	firewall-cmd --permanent --add-port=30000-32767/tcp
+	firewall-cmd --reload
+
+
+
+
+printf "overlay \nbr_netfilter" > /etc/modules-load.d/k8s.conf
+
+modprobe overlay
+modprobe br_netfilter
+
+printf "net.bridge.bridge-nf-call-iptables  = 1 \nnet.ipv4.ip_forward                 = 1 \nnet.bridge.bridge-nf-call-ip6tables = 1" > /etc/sysctl.d/k8s.conf
+sysctl --system
+
+export VERSION=1.21
+
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/CentOS_8/devel:kubic:libcontainers:stable.repo
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/CentOS_8/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo
+
+dnf install cri-o
+systemctl enable cri-o
+systemctl start cri-o
+
+printf "[kubernetes] \nname=Kubernetes \nbaseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64 \nenabled=1 \ngpgcheck=1 \nrepo_gpgcheck=1 \ngpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg\n       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg \nexclude=kubelet kubeadm kubectl" > /etc/yum.repos.d/kubernetes.repo
+
+
+dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+systemctl enable kubelet
+systemctl start kubelet
+
+
+		
+	
+
+
+#https://www.linuxtechi.com/how-to-install-kubernetes-cluster-rhel/
---
+++ worker.join.sh
@@ -0,0 +1,2 @@
+kubeadm join 10.10.10.101:6443 --token tqebqd.c5mrkc074tas63h1 \
+        --discovery-token-ca-cert-hash sha256:ea49b8f13327121cb8da119bf0ae2ee740da9c4ee15adf347e76d301a10982d7
