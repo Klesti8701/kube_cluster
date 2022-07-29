a35fcb4459d0074f3c465504f0e8e1f821100490
Klesti8701 <kfetiu19@beder.edu.al>
2022-07-29 16:04:32 +0200

    containerd-conf

--- Klesti8701-debian/master-node-apt.sh
+++ Klesti8701-debian/master-node-apt.sh
@@ -59,6 +59,26 @@ sudo sed -i 's/^/#&/' /etc/initramfs-tools/conf.d/resume
 #edit the containerd configs 
 sudo sed 's/"cri"//' /etc/containerd/config.toml
 
+#config kernal communication modules 
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+
 #run the init command
 ip=$(ip a | grep "scope global" | grep -Po '(?<=inet )[\d.]+')
 printf "%s\n" "$ip"
